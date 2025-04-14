6082d1f65bd3fa08bd8c134ad08ca13d53952668
Ajithpai21 <ajithpai71@gmail.com>
2025-04-14 12:34:53 +0530

    new change

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
+// Polyfill crypto.getRandomValues for Node.js
+globalThis.crypto ??= require('crypto').webcrypto;
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';  
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-})
+});
